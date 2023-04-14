98c83869f13549ca229cdb9b530821c27cdd833d
Andrea Vaccaro <100683661+ANDRVV@users.noreply.github.com>
2023-04-14 18:01:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 DESCRIPTION = 'FCP is a protocol used to route on "channels" and send or receive data.'
 
 setup(
